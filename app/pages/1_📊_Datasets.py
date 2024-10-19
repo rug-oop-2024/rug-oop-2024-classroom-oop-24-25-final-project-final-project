3e3b26cfd8f09c42c7c6906472083b099d6818b9
Jia Hao Zhang <j.h.zhang@student.rug.nl>
2024-10-19 16:49:14 +0200

    added simple table that will show the available datasets

--- app/pages/1_📊_Datasets.py
+++ app/pages/1_📊_Datasets.py
@@ -9,3 +9,16 @@ automl = AutoMLSystem.get_instance()
 datasets = automl.registry.list(type="dataset")
 
 # your code here
+
+datasets_name = pd.DataFrame({"Datasets": [art.name for art in datasets]}
+                             )
+
+st.set_page_config(page_title="Datasets"
+                   )
+
+st.title("Dataset")
+
+st.dataframe(datasets_name,
+             column_config={"Datasets": "Name datasets"},
+             hide_index=True
+             )
