44d4af79052a206d3a74b87ed1aa16e6753839a5
Jia Hao Zhang <j.h.zhang@student.rug.nl>
2024-10-21 12:37:54 +0200

    made create file. not shit appear when uploading a file

---
+++ app/datasets/management/create.py
@@ -0,0 +1,2 @@
+def create(file):
+    pass
--- app/pages/1_📊_Datasets.py
+++ app/pages/1_📊_Datasets.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 
 from app.core.system import AutoMLSystem
+from app.datasets.management.create import create
 from autoop.core.ml.dataset import Dataset
 
 automl = AutoMLSystem.get_instance()
@@ -10,15 +11,25 @@ datasets = automl.registry.list(type="dataset")
 
 # your code here
 
-datasets_name = pd.DataFrame({"Datasets": [art.name for art in datasets]}
-                             )
+datasets_display_dat = pd.DataFrame({"Datasets": [art.name for
+                                                  art in datasets],
+                                     "versions": [dataset.version for
+                                                  dataset in datasets]}
+                                    )
 
 st.set_page_config(page_title="Datasets"
                    )
 
 st.title("Dataset")
 
-st.dataframe(datasets_name,
-             column_config={"Datasets": "Name datasets"},
+new_dataset = st.file_uploader(label="Upload dataset(csv)",
+                               accept_multiple_files=False,
+                               type=["csv"])
+
+if new_dataset is not None:
+    st.write("shit💩")
+
+st.dataframe(datasets_display_dat,
+             column_config={"Datasets": "Name datasets", "versions": "version"},
              hide_index=True
              )
