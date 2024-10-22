16b89f2d2d254a6399fe8023fac577b10fbff424
Jia Hao Zhang <j.h.zhang@student.rug.nl>
2024-10-22 18:08:30 +0200

    added docstring to create

--- app/datasets/management/create.py
+++ app/datasets/management/create.py
@@ -4,6 +4,15 @@ from typing import IO
 
 
 def create(file: IO) -> Dataset:
+    """
+    Get uploaded file and converts it to a dataset.
+
+    Arguments:
+        file (IO): uploaded file from st file uploader
+
+    returns:
+        dataset (Dataset)
+    """
     dataset = Dataset(data=pd.read_csv(file).to_csv(index=False).encode(),
                       name=file.name)
     return dataset
