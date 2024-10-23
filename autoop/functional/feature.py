6d0e444341e65b669fb9949294df4c0f4f2d076b
LukaszSawala <lukaszsawala2003@gmail.com>
2024-10-23 11:52:44 +0200

    added feature class

--- autoop/functional/feature.py
+++ autoop/functional/feature.py
@@ -1,20 +1,43 @@
-from typing import List
+from typing import List, Literal
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.feature import Feature
 
 
-def detect_feature_types(dataset: Dataset) -> List[Feature]:
-    """Assumption: only categorical and numerical features and no NaN values.
-    Args:
-        dataset: Dataset
-    Returns:
-        List[Feature]: List of features with their types.
-    """
-    ds = dataset.read()
-    features = []
-    for name in ds.columns:
-        if ds[name].dtype == "object":
-            features.append(Feature(name=name, type="categorical"))
-        else:
-            features.append(Feature(name=name, type="numerical"))
-    return features
+class Feature:
+    """A class used to represent features of a dataset."""
+
+    def __init__(self, name: str, type: Literal["categorical", "numerical"]):
+        self._name = name
+        self._type = type
+
+    @property
+    def name(self) -> str:
+        """Getter for the name of the feature."""
+        return self._name
+
+    @property
+    def type(self) -> Literal["categorical", "numerical"]:
+        """Getter for the type of the feature."""
+        return self._type
+
+    def __str__(self) -> str:
+        """ String representation of the object """
+        return f"Feature name={self.name}, type={self.type}"
+
+    @staticmethod
+    def detect_feature_types(dataset: Dataset) -> List[Feature]:
+        """
+        Detects feature type of a dataset.
+        Bases on the assumption that all features are either
+        categorical or numerical, and no empty features are present.
+        :param dataset: Dataset object, storing the data
+        :return: List of features
+        """
+        ds = dataset.read()
+        features = []
+        for name in ds.columns:
+            if ds[name].dtype == "object":
+                features.append(Feature(name=name, type="categorical"))
+            else:
+                features.append(Feature(name=name, type="numerical"))
+        return features
