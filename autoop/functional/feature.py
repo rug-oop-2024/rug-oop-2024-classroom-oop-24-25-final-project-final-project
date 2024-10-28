5ed65da6f9e2d8404836487a721c5d54db9739e8
atodorov284 <atodorov284@gmail.com>
2024-10-28 19:28:41 +0100

    update

--- autoop/functional/feature.py
+++ autoop/functional/feature.py
@@ -1,7 +1,7 @@
+
 from typing import List
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.feature import Feature
-
 
 def detect_feature_types(dataset: Dataset) -> List[Feature]:
     """Assumption: only categorical and numerical features and no NaN values.
@@ -10,11 +10,4 @@ def detect_feature_types(dataset: Dataset) -> List[Feature]:
     Returns:
         List[Feature]: List of features with their types.
     """
-    ds = dataset.read()
-    features = []
-    for name in ds.columns:
-        if ds[name].dtype == "object":
-            features.append(Feature(name=name, type="categorical"))
-        else:
-            features.append(Feature(name=name, type="numerical"))
-    return features
+    raise NotImplementedError("This should be implemented by you.")
